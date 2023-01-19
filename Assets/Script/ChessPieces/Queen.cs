1f5efb55313510d3905eeb7a3aea784222bdef97
KrishPatel143 <aiie14@aii.ac.in>
2023-01-19 22:34:35 +0530

    message

--- Assets/Script/ChessPieces/Queen.cs
+++ Assets/Script/ChessPieces/Queen.cs
@@ -5,4 +5,117 @@ using UnityEngine;
 public class Queen : ChessPiece
 {
 
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] bord, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+
+        for (int i = currentY - 1; i >= 0; i--)
+        {
+            if (bord[currentX, i] != null)
+            {
+                if (team != bord[currentX, i].team)
+                {
+                    r.Add(new Vector2Int(currentX, i));
+                }
+                break;
+            }
+            r.Add(new Vector2Int(currentX, i));
+        }
+        for (int i = currentY + 1; i < tileCountY; i++)
+        {
+            if (bord[currentX, i] != null)
+            {
+                if (team != bord[currentX, i].team)
+                {
+                    r.Add(new Vector2Int(currentX, i));
+                }
+                break;
+            }
+            r.Add(new Vector2Int(currentX, i));
+        }
+        for (int i = currentX - 1; i >= 0; i--)
+        {
+            if (bord[i, currentY] != null)
+            {
+                if (team != bord[i, currentY].team)
+                {
+                    r.Add(new Vector2Int(i, currentY));
+                }
+                break;
+            }
+            r.Add(new Vector2Int(i, currentY));
+        }
+        for (int i = currentX + 1; i < tileCountY; i++)
+        {
+            if (bord[i, currentY] != null)
+            {
+                if (team != bord[i, currentY].team)
+                {
+                    r.Add(new Vector2Int(i, currentY));
+                }
+                break;
+            }
+            r.Add(new Vector2Int(i, currentY));
+        }
+
+                int x = currentX - 1; 
+        int y = currentY - 1;
+        while( x >=  0 && y >= 0){
+            if (bord[x, y] != null)
+            {
+                if (team != bord[x, y].team)
+                    r.Add(new Vector2Int(x, y));
+
+                break;
+            }
+            r.Add(new Vector2Int(x, y));
+
+            x--;
+            y--;
+        }
+        x = currentX + 1; 
+        y = currentY + 1;
+        while( x <  tileCountX && y < tileCountY){
+            if (bord[x, y] != null)
+            {
+                if (team != bord[x, y].team)
+                    r.Add(new Vector2Int(x, y));
+
+                break;
+            }
+            r.Add(new Vector2Int(x, y));
+            x++;
+            y++;
+        }
+        x = currentX - 1; 
+        y = currentY + 1;
+        while(x >= 0 && y < tileCountY){
+            if (bord[x, y] != null)
+            {
+                if (team != bord[x, y].team)
+                    r.Add(new Vector2Int(x, y));
+
+                break;
+            }
+            r.Add(new Vector2Int(x, y));
+            x--;
+            y++;
+        }
+        x = currentX + 1; 
+        y = currentY - 1;
+        while( x <  tileCountX && y >= 0){
+            if (bord[x, y] != null)
+            {
+                if (team != bord[x, y].team)
+                    r.Add(new Vector2Int(x, y));
+
+                break;
+            }
+            r.Add(new Vector2Int(x, y));
+            x++;
+            y--;
+        }
+
+        return r;
+    }
 }
